75d76407c83ae7d18ced8068cbad7ec12de6f544
ushvarma <ushvarma@gmail.com>
2020-08-17 13:39:15 -0400

    Updated unit tests

--- test/tests.py
+++ test/tests.py
@@ -2,10 +2,9 @@ import unittest
 
 import code
 
-
 class TestMethods(unittest.TestCase):
-    def test_answer(self):
-    	self.assertEqual(code.inc(3), 4)
+    def test_answer():
+    	assert code.inc(3) == 4
 
 if __name__ == '__main__':
     unittest.main() 
