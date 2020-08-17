773affb6de5a05198ec7318cca5d9a7cc5c3b37e
ushvarma <ushvarma@gmail.com>
2020-08-17 12:13:28 -0400

    Added tests.py file to the test folder

--- test/tests.py
+++ test/tests.py
@@ -4,8 +4,11 @@ import src
 
 
 class TestMethods(unittest.TestCase):
-    def test_add(self):
+    def test_happy(self):
         self.assertEqual(src.happy(), ":)")
+
+    def test_sad(self):
+    	self.assertEqual(src.sad(), ":(")
 
 
 if __name__ == '__main__':
